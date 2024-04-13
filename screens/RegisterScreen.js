610636651e38ef27daded43061a112050b5add8c
Elif Duygu Demircioğlu <95955738+elifedd@users.noreply.github.com>
2024-04-13 16:12:09 +0300

    temporary ui improvements for the auth

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -5,6 +5,7 @@ import {
   TextInput,
   Button,
   ActivityIndicator,
+  TouchableOpacity,
 } from 'react-native';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import Colors from '../constants/colors';
@@ -59,37 +60,52 @@ function RegisterScreen({ navigation }) {
       </View>
       <View style={styles.bottomContainer}>
         <Text style={styles.screenText}>{t('signup')}</Text>
-        <Text>Your Name</Text>
-        <TextInput
-          value={username}
-          placeholder='Full name'
-          autoCapitalize='none'
-          onChangeText={(text) => setUsername(text)}
-        ></TextInput>
-        <Text>Your Email</Text>
-        <TextInput
-          value={email}
-          placeholder='Email'
-          autoCapitalize='none'
-          onChangeText={(text) => setEmail(text)}
-        ></TextInput>
-        <Text>Your Password</Text>
-        <TextInput
-          secureTextEntry={true}
-          value={password}
-          placeholder='Password'
-          autoCapitalize='none'
-          onChangeText={(text) => setPassword(text)}
-        ></TextInput>
+        <View>
+          <Text style={styles.textInputLabel}>Your Name</Text>
+          <TextInput
+            style={styles.textInput}
+            value={username}
+            placeholder='Full name'
+            autoCapitalize='none'
+            onChangeText={(text) => setUsername(text)}
+          ></TextInput>
+        </View>
+        <View>
+          <Text style={styles.textInputLabel}>Your Email</Text>
+          <TextInput
+            style={styles.textInput}
+            value={email}
+            placeholder='Email'
+            autoCapitalize='none'
+            onChangeText={(text) => setEmail(text)}
+          ></TextInput>
+        </View>
+        <View>
+          <Text style={styles.textInputLabel}>Your Password</Text>
+          <TextInput
+            style={styles.textInput}
+            secureTextEntry={true}
+            value={password}
+            placeholder='Password'
+            autoCapitalize='none'
+            onChangeText={(text) => setPassword(text)}
+          ></TextInput>
+        </View>
         {loading ? (
           <ActivityIndicator size='large' color='#0000ff' />
         ) : (
           <>
-            <Button title='Create account' onPress={() => signUp()}></Button>
-            <Button
-              title='Back'
+            <TouchableOpacity style={styles.button} onPress={() => signUp()}>
+              <Text style={styles.buttonText}>Create account</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonOutline]}
               onPress={() => navigation.navigate('Login')}
-            ></Button>
+            >
+              <Text style={[styles.buttonText, styles.buttonOutlineText]}>
+                Back
+              </Text>
+            </TouchableOpacity>
           </>
         )}
       </View>
@@ -121,6 +137,7 @@ const styles = StyleSheet.create({
   },
   bottomContainer: {
     flex: 5,
+    gap: 10,
     width: '100%',
     backgroundColor: 'white',
     borderTopLeftRadius: 20,
@@ -128,5 +145,43 @@ const styles = StyleSheet.create({
     paddingVertical: 48,
     paddingHorizontal: 24,
     elevation: 4,
+  },
+  textInput: {
+    paddingVertical: 12,
+    paddingHorizontal: 18,
+    borderWidth: 1,
+    borderColor: Colors.lightGray,
+    borderRadius: 10,
+    color: Colors.navy,
+    fontFamily: 'Poppins-Regular',
+    textAlignVertical: 'center',
+    alignItems: 'center',
+  },
+  textInputLabel: {
+    fontFamily: 'Poppins-Medium',
+    color: Colors.navy,
+    fontSize: 14,
+    marginBottom: 6,
+  },
+  button: {
+    paddingVertical: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.green,
+    borderRadius: 10,
+  },
+  buttonOutline: {
+    backgroundColor: Colors.white,
+    borderColor: Colors.green,
+    borderWidth: 1,
+  },
+  buttonText: {
+    color: Colors.white,
+    fontSize: 16,
+    lineHeight: 20,
+    fontFamily: 'Poppins-Medium',
+  },
+  buttonOutlineText: {
+    color: Colors.green,
   },
 });
