8c78ba81515c9eda267090b4c6128130280e6fae
cat1lina <zml44578@gmail.com>
2024-04-13 21:31:24 +0300

    homejs resolve

--- screens/Home.js
+++ screens/Home.js
@@ -1,60 +1,85 @@
-import { View, Button, FlatList } from 'react-native';
-import { collection, getDocs } from 'firebase/firestore';
-import { FIREBASE_FIRESTORE } from '../FirebaseConfig';
-import FoodItem from '../components/FoodItem';
-import { useState, useEffect, useCallback } from 'react';
-import { useFocusEffect } from '@react-navigation/native';
+import { View, StyleSheet, ScrollView, Button } from 'react-native';
+import { useState, useEffect, useContext } from 'react';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import CategoryList from '../components/CategoryList';
+import GlobalApi from '../src/services/GlobalApi';
+import PlaceList from '../components/PlaceList';
+import { UserLocationContext } from '../src/context/UserLocationContext';
+import { SIZES } from '../constants/sizes';
 
 function Home({ navigation }) {
-  const [foodItems, setFoodItems] = useState([]);
+  const [placeList, setPlaceList] = useState([]);
+  const { location, setLocation } = useContext(UserLocationContext);
+  const mapRegion = {
+    latitude: location.coords.latitude,
+    longitude: location.coords.longitude,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0421,
+  };
 
-  const fetchData = async () => {
-    try {
-      const querySnapshot = await getDocs(
-        collection(FIREBASE_FIRESTORE, 'FoodPost')
-      );
-      const fetchedData = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        providerId: doc.data().PostFoodProvider,
-        ...doc.data(),
-      }));
-      setFoodItems([...fetchedData]); // Update the state with the newly fetched data
-    } catch (error) {
-      console.error('Error fetching data:', error);
+  const GetNearBySearchPlace = (value) => {
+    if (location) {
+      GlobalApi.nearByPlace(
+        location.coords.latitude,
+        location.coords.longitude,
+        value
+      )
+        .then((resp) => {
+          setPlaceList(resp.data.results);
+        })
+        .catch((error) => {
+          console.error('Error fetching nearby places:', error);
+        });
+
     }
   };
 
-  // Fetch data when component mounts
   useEffect(() => {
-    fetchData();
-  }, []);
+    GetNearBySearchPlace('restaurant');
+  }, [location]);
 
-  // Fetch data when screen focuses (navigated back from another screen)
-  useFocusEffect(
-    useCallback(() => {
-      fetchData();
-    }, [])
-  );
+  // const renderItem = ({ item }) => <FoodItem data={item} />;
 
-  const renderItem = ({ item }) => <FoodItem data={item} navigation={navigation} />;
 
   return (
-    <View style={{ flex: 1 }}>
-      <FlatList
-        data={foodItems}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
+    <ScrollView style={styles.container}>
+      <View>
+        {location && (
+          <MapView
+            style={styles.map}
+            provider={PROVIDER_GOOGLE}
+            showsUserLocation={true}
+            region={mapRegion}
+          >
+            <Marker title='You' coordinate={mapRegion} />
+          </MapView>
+        )}
+      </View>
+      <CategoryList
+        setSelectedCategory={(value) => GetNearBySearchPlace(value)}
       />
-      <Button
-        onPress={() => navigation.navigate('FoodPost')}
-        title='Open Food Post'
-      />
-      <Button
-        onPress={() => navigation.navigate('Details')}
-        title='Open Details'
-      />
-    </View>
+      {placeList && <PlaceList placeList={placeList} />}
+      <View style={styles.button}>
+        <Button
+          onPress={() => navigation.navigate('FoodPost')}
+          title='Open Food Post'
+        />
+      </View>
+    </ScrollView>
   );
 }
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  map: {
+    height: 200,
+    width: SIZES.width - 40,
+  },
+  button: {
+    marginBottom: 40,
+  },
+});
