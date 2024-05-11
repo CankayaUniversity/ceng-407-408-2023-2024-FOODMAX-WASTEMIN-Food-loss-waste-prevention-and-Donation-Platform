c272cb1c98ae22b37d63ee7ebb290632654c5b27
Elif Duygu Demircioğlu <95955738+elifedd@users.noreply.github.com>
2024-05-11 10:27:11 +0300

    implement ui for settings page

---
+++ components/CustomSettingsButton.js
@@ -0,0 +1,56 @@
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import Colors from '../constants/colors';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const CustomSettingsButton = ({
+  onPress,
+  iconBefore,
+  title,
+  style,
+  textStyle,
+  iconColor = Colors.navy,
+}) => {
+  return (
+    <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
+      {iconBefore && (
+        <MaterialIcons
+          name={iconBefore}
+          size={24}
+          style={[styles.iconBefore]}
+          color={iconColor}
+        />
+      )}
+      <Text style={[styles.buttonText, textStyle]}>{title}</Text>
+      <MaterialIcons
+        name='chevron-right'
+        size={24}
+        style={[styles.iconAfter, style]}
+        color={iconColor}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export default CustomSettingsButton;
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  buttonText: {
+    color: Colors.navy,
+    fontSize: 16,
+    textAlign: 'left',
+  },
+  iconAfter: {
+    marginLeft: 'auto',
+  },
+  iconBefore: {
+    marginRight: 8,
+  },
+});
--- screens/Settings.js
+++ screens/Settings.js
@@ -1,23 +1,53 @@
-import { View, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { FIREBASE_AUTH } from '../FirebaseConfig';
 import { useNavigation } from '@react-navigation/native';
+import CustomSettingsButton from '../components/CustomSettingsButton';
+import Colors from '../constants/colors';
 
 function Settings() {
   const navigation = useNavigation();
 
   return (
     <View>
-      <Button
-        onPress={() => navigation.navigate('ProfileScreen')}
-        title='Profile Settings'
-      />
-      <Button
-        onPress={() => navigation.navigate('StoreScreen')}
-        title='Store Login'
-      />
-      <Button onPress={() => FIREBASE_AUTH.signOut()} title='Logout' />
+      <View style={styles.container}>
+        <CustomSettingsButton
+          onPress={() => navigation.navigate('ProfileScreen')}
+          title='Profile Settings'
+          style={styles.buttonWidthBorder}
+          iconBefore='settings'
+        />
+        <CustomSettingsButton
+          onPress={() => navigation.navigate('StoreScreen')}
+          title='Store Login'
+          iconBefore='store'
+        />
+      </View>
+      <View style={styles.container}>
+        <CustomSettingsButton
+          onPress={() => FIREBASE_AUTH.signOut()}
+          title='Logout'
+          iconBefore='logout'
+          iconColor='red'
+          textStyle={styles.customColorButton}
+        />
+      </View>
     </View>
   );
 }
 
 export default Settings;
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    backgroundColor: Colors.white,
+    borderRadius: 10,
+  },
+  buttonWidthBorder: {
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.lightGray,
+  },
+  customColorButton: {
+    color: 'red',
+  },
+});
