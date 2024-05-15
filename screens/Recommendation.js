7b0bed32f7ac3f1f2a1535c1c95311480892191f
elfnursenturk <134106153+elfnursenturk@users.noreply.github.com>
2024-05-15 18:02:52 +0300

    Edit Recommendation Screen

--- screens/Recommendation.js
+++ screens/Recommendation.js
@@ -52,7 +52,6 @@ export default function Recommendation() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //justifyContent: 'center',
     alignItems: 'center',
   },
   title: {
